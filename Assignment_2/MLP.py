e010b641ffc7c4f57a6b30f4bdafc6b2a359fb6f
Anastasios Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2023-01-17 13:15:56 +0200

    Third part

--- Assignment_2/MLP.py
+++ Assignment_2/MLP.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+import fasttext
 import numpy as np
 
 def main(n, m, k):
     index_from = n
     seed = 113
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(index_from=index_from, seed=seed)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data()
     word_index = tf.keras.datasets.imdb.get_word_index()
     index2word = dict((i+3, word) for (word, i) in word_index.items())
     index2word[0] = '[pad]'
@@ -18,8 +19,15 @@ def main(n, m, k):
     vectorizer = tf.keras.layers.TextVectorization(max_tokens=m, output_mode='int', ngrams=1, name='vector_text', output_sequence_length=avg_length)
     vectorizer.adapt(x_train)
 
-    model = rnn(vectorizer)
-    model.compile(loss=tf.keras.BinaryCrossentropy(), optimizer=tf.keras.optimizers.SGD(), metrics=['binary_accuracy'])
+    fasttext_model = fasttext.load_model('cc.en.300.bin')
+    embeddings_matrix = np.zeros(shape=(len(vectorizer.get_vocabulary()), 300))
+
+    for i, word in enumerate(vectorizer.get_vocabulary()):
+        embeddings_matrix[i] = fasttext_model.get_word_vector(word=word)
+    del fasttext_model
+
+    model = rnn(vectorizer, embeddings_matrix, emb_size=300)
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(), metrics=['binary_accuracy'])
     model.fit(x=x_train, y=y_train, epochs=10, verbose=1, batch_size=32)
     print(model.evaluate(x_test, y_test))
 
@@ -28,14 +36,16 @@ def get_average_length(x_train):
     for example in x_train: avg_len += len(str(example).split())
     return int(avg_len / len(x_train))
 
-def rnn(vectorizer, num_layers=1, emb_size=64, h_size=64):
+def rnn(vectorizer, embeddings_matrix, num_layers=1, emb_size=64, h_size=64):
     inputs = tf.keras.layers.Input(shape=(1,), dtype=tf.string, name='txt_input')
     x = vectorizer(inputs)
     x = tf.keras.layers.Embedding(
                                     input_dim=len(vectorizer.get_vocabulary()),
                                     output_dim=emb_size, name='word_embeddings',
+                                    trainable=False, weights=[embeddings_matrix],
                                     mask_zero=True)(x)
     
+    x = tf.keras.layers.Dropout(rate=0.25)(x)
     for n in range(num_layers):
         if n != num_layers - 1:
             x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(units=h_size,
