2361586fbcc4b3255ff8851670c1bd8a3d2dbf55
Anastasios Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2023-01-29 17:18:42 +0200

    Changed to multiprocessing instead of multithreading:  - Multithreading had problems with allocating memory

--- Assignment_2/random_search.py
+++ Assignment_2/random_search.py
@@ -1,6 +1,6 @@
 from random import sample
 from time import sleep
-from threading import Thread
+from multiprocessing import Process, Manager
 from tqdm import tqdm
 from utils import *
 from MLP import rnn
@@ -14,74 +14,61 @@ import itertools
 # n, m, k
 # Logistic regression: learning_rate, lambda
 n = k = 15000
+learning_rate = 0.01
 def generate_random_numbers(n, low, upper, step):
     nums = [round(val, 4) for val in list(np.arange(low, upper + step, step))]
     return sorted(sample(nums, n))
 
-def get_combinations(n_m, n_learning_rate, n_lambda):
+def get_combinations(n_m, n_lambda):
     random_m = generate_random_numbers(n_m, 1000, 20000, 1000)
 
-    random_learning_rates = generate_random_numbers(n_learning_rate, 0.005, 0.05, 0.001)
     random_lambda = generate_random_numbers(n_lambda, 0.001, 0.01, 0.001)
-    combinations = list(itertools.product(random_m, random_learning_rates, random_lambda))
+    combinations = list(itertools.product(random_m, random_lambda))
 
     return combinations
 
-def thread_search(combinations):
-    print('Starting')
+def process_search(combinations, return_list):
+    print('Starting process')
     min_mean_error = 1
     best_combination = None
     val_size = 0.2
 
     for combination in combinations:
-        m, lr, l = combination
+        m, l = combination
         train, _ = preprocess_data(n, m, k)
 
         x_train, x_test , y_train, y_test = train_test_split(train[1], train[2], test_size=val_size)
         bayes_error = evaluate_bayes((x_train, y_train), (x_test, y_test), 100, True)
-        log_reg_error = log_reg.evaluate_logistic_regression((x_train, y_train), (x_test, y_test), 100, 100, lr, l, True)
-        #x_train, x_test, y_train, y_test = train_test_split(train[0], train[2], test_size=val_size)
-        #rnn_error = rnn((x_train, y_train), (x_test, y_test), m, 100, 10, True)
+        log_reg_error = log_reg.evaluate_logistic_regression((x_train, y_train), (x_test, y_test), 100, 100, learning_rate, l, True)
+        x_train, x_test, y_train, y_test = train_test_split(train[0], train[2], test_size=val_size)
+        rnn_error = rnn((x_train, y_train), (x_test, y_test), m, 100, 10, True)
 
-        curr_min_error = (bayes_error + log_reg_error) / 2
+        curr_min_error = (bayes_error + log_reg_error + rnn_error) / 3
         if curr_min_error < min_mean_error:
             min_mean_error = curr_min_error
-            best_combination = (n, k, m, lr, l)
+            best_combination = (n, k, m, learning_rate, l)
     
-    return 1 - min_mean_error, best_combination
+    return_list.append((1 - min_mean_error, best_combination))
     
+def search(num_of_processes):
+    combinations = get_combinations(6, 6)
+    manager = Manager()
+    return_list = manager.list()
 
-class CustomThread(Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-    
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
-    
-    def join(self):
-        Thread.join(self)
-        return self._return
-    
-def test(combinations):
-    return combinations
-
-def search(num_of_threads):
-    combinations = get_combinations(1, 2, 2)
-
-    threads = []
-    step = len(combinations) // num_of_threads
-    for i in range(num_of_threads):
-        if i != num_of_threads - 1: curr_combinations = combinations[i*step : (i+1)*step]
+    processes = []
+    step = len(combinations) // num_of_processes
+    for i in range(num_of_processes):
+        if i != num_of_processes - 1: curr_combinations = combinations[i*step : (i+1)*step]
         else: curr_combinations = combinations[i*step:]
-        threads.append(CustomThread(target=thread_search, args=[curr_combinations]))
+        p = Process(target=process_search, args=(curr_combinations, return_list))
+        processes.append(p)
+        p.start()
 
-    for thread in threads:
-        thread.start()
+    for process in processes:
+        process.join()
     
-    results = [thread.join(timeout=3600) for thread in threads]
-    min_idx = results.index(max(results, key=lambda x: x[0]))
-    print(results[min_idx])
+    max_idx = return_list.index(max(return_list, key=lambda x: x[0]))
+    print(return_list[max_idx])
 
-search(4)
+if __name__ == '__main__':
+    search(9)
